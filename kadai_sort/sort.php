cb7767cecc9eb7ab4b4da1e5cb388b0ab12bad17
kareni136 <jsfwm206@gmail.com>
2024-08-26 16:59:06 +0900

    kadai_sort

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -11,31 +11,30 @@
       <?php
           // 独自のソート関数を定義
           function sort_2way(&$array, $order) {
+              // ソート方向に基づいて配列をソート
               if ($order) {
                   asort($array);  // 昇順にソート
+                  echo "昇順にソートします。<br>";
               } else {
                   arsort($array); // 降順にソート
+                  echo "降順にソートします。<br>";
               }
+
+              // ソートされた配列の要素を出力
+              foreach($array as $val) {
+                  echo $val . "<br>";
+              }
+              echo "<br>"; // 間にスペースを挿入
           }
 
           // ソートする配列を宣言
           $nums = [15, 4, 18, 23, 10];
 
           // 昇順ソートを呼び出し
-          echo "昇順にソートします。<br>";
           sort_2way($nums, true);
-          foreach($nums as $val) {
-              echo $val . "<br>";
-          }
-
-          echo "<br>"; // 間にスペースを挿入
 
           // 降順ソートを呼び出し
-          echo "降順にソートします。<br>";
           sort_2way($nums, false);
-          foreach($nums as $val) {
-              echo $val . "<br>";
-          }
       ?>
     </p>
 </body>
